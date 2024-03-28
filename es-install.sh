9fc6ef94a6b1b769112615b142104e400a854e9d
Ali Ibrahim <loosh888@gmail.com>
2024-03-28 14:05:38 +0300

    Add files via upload

---
+++ es-install.sh
@@ -0,0 +1,18 @@
+file="/etc/yum.repos.d/elasticsearch.repo"
+
+echo "[elasticsearch]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=0
+autorefresh=1
+type=rpm-md" > $file
+
+sudo yum install --enablerepo=elasticsearch elasticsearch-8.2.0
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+sleep 1m
+sudo systemctl status elasticsearch.service
+curl --cacert /etc/elasticsearch/certs/http_ca.crt -u elastic:$ELASTIC_PASSWORD https://localhost:9200
